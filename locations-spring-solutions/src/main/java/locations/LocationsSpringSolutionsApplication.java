06f4f24b6fe20f00f2f1fe881934869ae3c5481f
NDboy <endre.karsai@gmail.com>
2021-07-04 15:31:33 +0200

    Konfiguráció Jávában

--- locations-spring-solutions/src/main/java/locations/LocationsSpringSolutionsApplication.java
+++ locations-spring-solutions/src/main/java/locations/LocationsSpringSolutionsApplication.java
@@ -2,12 +2,18 @@ package locations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LocationsSpringSolutionsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LocationsSpringSolutionsApplication.class, args);
+    }
+
+    @Bean
+    public LocationService locationService() {
+        return new LocationService();
     }
 
 }
