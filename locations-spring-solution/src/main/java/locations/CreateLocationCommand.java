d320ceeb6fcf6ea796c11223d566b74e91ab857b
NDboy <endre.karsai@gmail.com>
2021-07-05 21:11:09 +0200

    Integrációs tesztelés -- RestTemplate

--- locations-spring-solution/src/main/java/locations/CreateLocationCommand.java
+++ locations-spring-solution/src/main/java/locations/CreateLocationCommand.java
@@ -2,9 +2,11 @@ package locations;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateLocationCommand {
 
     private String name;
---
+++ locations-spring-solution/src/test/java/locations/LocationControllerRestTemplateIT.java
@@ -0,0 +1,70 @@
+package locations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LocationControllerRestTemplateIT {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    void testListLocations() {
+        LocationDto locationDto = testRestTemplate.postForObject("/api/locations", new CreateLocationCommand("Puerto Rico", -18.00, -66.1234), LocationDto.class);
+        assertEquals("Puerto Rico", locationDto.getName());
+
+        testRestTemplate.postForObject("/api/locations", new CreateLocationCommand("Lima", -12.02, -77.02), LocationDto.class);
+
+        List<LocationDto> locationDtos = testRestTemplate.exchange("/api/locations",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<LocationDto>>() {})
+                .getBody();
+        assertThat(locationDtos)
+                .hasSize(4)
+                .extracting(LocationDto::getName, LocationDto::getLat)
+                .contains(tuple("Puerto Rico", -18.00), tuple("Lima", -12.02));
+    }
+
+    @Test
+    void testFindLocationById() {
+        LocationDto locationDto = testRestTemplate.getForObject("/api/locations/{id}", LocationDto.class, 1);
+        assertEquals("Buenos Aires", locationDto.getName());
+        LocationDto locationDto2 = testRestTemplate.getForObject("/api/locations/{id}", LocationDto.class, 2);
+        assertEquals("Brasil", locationDto2.getName());
+    }
+
+    @Test
+    void testUpdateLocation() {
+        testRestTemplate.put("/api/locations/{id}", new UpdateLocationCommand("Buenos Aires", -34.12144, -58.44), 1);
+        LocationDto locationDto = testRestTemplate.getForObject("/api/locations/{id}", LocationDto.class, 1);
+        assertEquals(-58.44, locationDto.getLon());
+    }
+
+    @Test
+    void testDeleteLocation() {
+        testRestTemplate.delete("/api/locations/{id}", 1);
+        List<LocationDto> locationDtos = testRestTemplate.exchange("/api/locations",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<LocationDto>>() {})
+                .getBody();
+        assertThat(locationDtos)
+                .hasSize(1)
+                .extracting(LocationDto::getName, LocationDto::getLat)
+                .containsOnly(tuple("Brasil", -39.12188));
+    }
+
+
+}
